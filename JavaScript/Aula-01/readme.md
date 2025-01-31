a432764cbfb5920218f32ec682eb0df05a0e8d8c
JulioFavoretti <julio3.cesar56@gmail.com>
2025-01-31 12:19:59 -0300

    Commit de JavaScript

---
+++ JavaScript/Aula-01/readme.md
@@ -0,0 +1,5 @@
+  ## Aula de introdução a JS - JavaScript
+
+  #### Declaração de variáveis 🤠.
+  #### Alert, Confirm, Prompt 🔮.
+  #### Questões aritméticas, console.
---
+++ JavaScript/Aula-01/script.js
@@ -0,0 +1,28 @@
+console.log("Olá Mundo");
+
+//Window.alert apenas para o primeiro, os próximos funcionam normalmente
+//window.alert("Olá Mundo 1");
+
+//Exemplo alert()
+    //alert("Olá Mundo 1");
+
+//Exemplo de confirm()
+    //var teste = confirm("Olá Mundo 2");
+    //Exibindo a opção escolhida pelo usuário.
+    //console.log(teste);
+
+//Exemplo de prompt()
+    //var testePrompt = prompt ("Digite o seu nome: ");
+    //exibindo o nome do usuário
+    //console.log(testePrompt);
+    //alert(testePrompt);
+
+//Tentativa de Adição
+    var numero1 = Number.parseInt (prompt ("Digite o primeiro numero: "));
+    var numero2 = Number.parseInt(prompt ("Digite o segundo numero: "));
+    //Verificando o tipo da variavel
+    console.log(typeof(numero1));
+    alert(numero1 + numero2);
+
+    
+
